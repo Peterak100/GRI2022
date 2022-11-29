810c626efcbc66394f6f848364dc1902cf8b6782
Peterak100 <peterkriesner@yahoo.com>
2022-11-29 11:40:04 +1100

    A couple of bug fixes

--- bulk_obs.R
+++ bulk_obs.R
@@ -1,4 +1,4 @@
-suppressMessages(source("R/common1.R"))
+suppressPackageStartupMessages(source("R/common1.R"))
 source("categorize3.R")
 source("obs4.R")
 source("resistance5.R")
--- precluster.R
+++ precluster.R
@@ -1,9 +1,9 @@
-source("R/common1.R")
+suppressPackageStartupMessages(source("R/common1.R"))
 source("categorize3.R")
 source("obs4.R")
 source("resistance5.R")
 source("postprocess7.R")
-precategorized_taxa1 <- read_csv(file.path(groupingspath,
+precategorized_taxa <- read_csv(file.path(groupingspath,
           "precategorized_taxa.csv"), show_col_types = FALSE)
 isolation_taxa <- dplyr::filter(precategorized_taxa,
           filter_category == 
